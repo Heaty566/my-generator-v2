7e4f7f881b84439a7848fb7d0f18113f5f1ef52d
Nhân Phạm <heaty566@gmail.com>
2021-03-17 08:23:33 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Btn from "./component/btn";
+import faker from "faker";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        const [text, setText] = useState("");
+        const [timer, setTimer] = useState(30);
+
+        useEffect(() => {
+                const interId = setInterval(() => {
+                        setTimer(timer - 1);
+                }, 1000);
+
+                if (timer <= 0) newText();
+
+                return () => {
+                        clearInterval(interId);
+                };
+        }, [timer]);
+
+        const newText = () => {
+                let str: string = "";
+                for (let index = 0; index < 200; index++) {
+                        str = str + " " + faker.random.words(1).toLowerCase();
+                }
+                setTimer(30);
+                setText(str);
+        };
+
+        useEffect(() => {
+                newText();
+        }, []);
+
+        return (
+                <div className="bg-indigo-300 text-white min-h-screen flex flex-col">
+                        <header className="bg-gray-800 h-16 flex items-center px-16 shadow-sm">
+                                <h1 className="text-2xl font-semibold">
+                                        <a href="/">My Generator</a>
+                                </h1>
+                        </header>
+                        <main className="flex-1 flex justify-center items-center">
+                                <div className="p-4 bg-gray-900 max-w-4xl rounded-sm space-y-8">
+                                        <div className="flex  space-x-4">
+                                                <Btn label="New" callback={() => newText()} icon="new" />
+                                                <Btn
+                                                        label="Copy"
+                                                        callback={() => {
+                                                                navigator.clipboard.writeText(text);
+                                                                newText();
+                                                        }}
+                                                        icon="copy"
+                                                />
+                                        </div>
+                                        <div className="p-2 border-gray-100 border-2 capitalize">{text}</div>
+                                        <div className="text-right font-semibold">
+                                                <p>Auto renew in {timer}s</p>
+                                        </div>
+                                </div>
+                        </main>
+                </div>
+        );
 }
 
 export default App;
---
+++ src/component/btn/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Icons } from "./style";
+
+export interface ButtonProps {
+        label: string;
+        callback: Function;
+        icon: keyof typeof Icons;
+}
+
+const Button: React.FunctionComponent<ButtonProps> = ({ label, callback, icon }) => {
+        return (
+                <button
+                        onClick={() => callback()}
+                        type="button"
+                        className="bg-white px-4 py-2 text-black  font-semibold flex justify-center items-center rounded-sm text-lg duration-300 hover:bg-gray-300 focus:outline-none"
+                >
+                        {label}
+                        <div className="ml-2">{Icons[icon]}</div>
+                </button>
+        );
+};
+
+export default Button;
