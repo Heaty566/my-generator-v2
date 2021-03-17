8a6c882cb324f217244625f0f6b1de7322c0c0cb
Nhân Phạm <heaty566@gmail.com>
2021-03-17 09:04:45 +0700

    done

--- src/component/btn/index.tsx
+++ src/component/btn/index.tsx
@@ -12,7 +12,7 @@ const Button: React.FunctionComponent<ButtonProps> = ({ label, callback, icon })
                 <button
                         onClick={() => callback()}
                         type="button"
-                        className="bg-white px-4 py-2 text-black  font-semibold flex justify-center items-center rounded-sm text-lg duration-300 hover:bg-gray-300 focus:outline-none"
+                        className="bg-white px-4 py-2 text-black  font-semibold flex justify-center items-center rounded-sm text-lg duration-300 hover:bg-blue-300 focus:outline-none"
                 >
                         {label}
                         <div className="ml-2">{Icons[icon]}</div>
